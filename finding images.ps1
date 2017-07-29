a794e40d578bf101abc48ca0a9eeff22f38508b0
Rob Sewell <mrrobsewell@outlook.com>
2017-07-29 18:26:52 +0100

    added finding imagfes

---
+++ finding images.ps1
@@ -0,0 +1,8 @@
+## FInding images
+
+$loc = Get-AzureRmLocation | OGV -passthru | select Location #first set a location
+#View the templates available
+$publisher=Get-AzureRmVMImagePublisher -Location $loc.Location |OGV -passthru | select publishername #check all the publishers available
+$offer=Get-AzureRmVMImageOffer -Location $loc.Location -PublisherName $publisher.PublisherName|OGV -passthru |select offer #look for offers for a publisher
+$sku=Get-AzureRmVMImageSku -Location $loc.Location -PublisherName $publisher.PublisherName -Offer $offer.Offer | OGV -passthru |select skus #view SKUs for an offer
+Get-AzureRmVMImage -Location $loc.Location -PublisherName $publisher.PublisherName -Offer $offer.Offer -Skus $sku.Skus #pick one!
