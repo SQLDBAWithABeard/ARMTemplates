590548daa38255580a0f4fbfc8fff1d9a3761fdf
Rob Sewell <mrrobsewell@outlook.com>
2018-04-03 16:15:39 +0100

    run the install script

--- DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
+++ DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
@@ -3,7 +3,13 @@ $Username = 'EnterpriseAdmin'
 
 $Password = $DomainAdminPassword | ConvertTo-SecureString -AsPlainText  -Force 
 $cred = New-Object System.Management.Automation.PSCredential $Username, $Password
+Write-Output "Creating PSSession"
 $so = New-PsSessionOption -SkipCACheck -SkipCNCheck
 $session = New-PSSession -ComputerName beardjumpbox.westeurope.cloudapp.azure.com -Credential $cred -UseSSL -SessionOption $so
 $Url = $ENV:JumpBoxSoftwareInstallscript
-Invoke-Command -Session $session -ScriptBlock {(New-Object System.Net.WebClient).DownloadFile($Using:Url, 'C:\Windows\Temp\SoftwareInstall.ps1')}
+Write-Output "Downloading File"
+$ICOuput = Invoke-Command -Session $session -ScriptBlock {(New-Object System.Net.WebClient).DownloadFile($Using:Url, 'C:\Windows\Temp\SoftwareInstall.ps1')}
+Write-Output $ICOutput
+Write-Output "Running Install Script"
+$ICOuput = Invoke-Command -Session $session -ScriptBlock{ 'C:\Windows\Temp\SoftwareInstall.ps1'}
+Write-Output $ICOutput
