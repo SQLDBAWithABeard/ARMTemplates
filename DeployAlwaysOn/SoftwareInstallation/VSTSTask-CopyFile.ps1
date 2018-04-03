95617f35d9f46a07d119f4ad7e07f3ddbb973010
Rob Sewell <mrrobsewell@outlook.com>
2018-04-03 15:21:43 +0100

    new session fool

--- DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
+++ DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
@@ -4,6 +4,6 @@ $Username = 'EnterpriseAdmin'
 $Password = $DomainAdminPassword | ConvertTo-SecureString -AsPlainText  -Force 
 $cred = New-Object System.Management.Automation.PSCredential $Username, $Password
 $so = New-PsSessionOption -SkipCACheck -SkipCNCheck
-$session = Enter-PSSession -ComputerName beardjumpbox.westeurope.cloudapp.azure.com -Credential $cred -UseSSL -SessionOption $so
+$session = New-PSSession -ComputerName beardjumpbox.westeurope.cloudapp.azure.com -Credential $cred -UseSSL -SessionOption $so
 $Url = $ENV:JumpBoxSoftwareInstallscript
-Invoke-Command -SessionName $session -ScriptBlock {(New-Object System.Net.WebClient).DownloadFile($Using:Url, 'C:\Windows\Temp\SoftwareInstall.ps1')}
+Invoke-Command -Session $session -ScriptBlock {(New-Object System.Net.WebClient).DownloadFile($Using:Url, 'C:\Windows\Temp\SoftwareInstall.ps1')}
