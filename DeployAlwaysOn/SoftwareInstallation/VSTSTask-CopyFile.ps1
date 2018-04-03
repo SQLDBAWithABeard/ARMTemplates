67e3f6f7271ec3d0742cc942187d9c1749c7fa90
Rob Sewell <mrrobsewell@outlook.com>
2018-04-03 15:06:21 +0100

    duff character

--- DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
+++ DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
@@ -3,7 +3,7 @@ $Username = 'EnterpriseAdmin'
 
 $Password = $DomainAdminPassword | ConvertTo-SecureString  -Force 
 $cred = New-Object System.Management.Automation.PSCredential $Username, $Password
-$so = New-PsSessionOption –SkipCACheck -SkipCNCheck
+$so = New-PsSessionOption -SkipCACheck -SkipCNCheck
 $session = Enter-PSSession -ComputerName beardjumpbox.westeurope.cloudapp.azure.com -Credential $cred -UseSSL -SessionOption $so
 $Url = $ENV:JumpBoxSoftwareInstallscript
 Invoke-Command -SessionName $session -ScriptBlock {(New-Object System.Net.WebClient).DownloadFile($Using:Url, 'C:\Windows\Temp\SoftwareInstall.ps1')}
