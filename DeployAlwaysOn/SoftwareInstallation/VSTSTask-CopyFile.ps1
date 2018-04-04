8b28a8a7499d0698d665c7ea720c27f53b1e788b
Rob Sewell <mrrobsewell@outlook.com>
2018-04-04 10:21:41 +0100

    removed credssp

--- DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
+++ DeployAlwaysOn/SoftwareInstallation/VSTSTask-CopyFile.ps1
@@ -2,10 +2,6 @@ Param($DomainAdminPassword)
 $VerbosePreference = 'Continue'
 $Username = 'EnterpriseAdmin'
 
-Enable-WSManCredSSP -Role server -Force
-Enable-WSManCredSSP -Role client -DelegateComputer beardjumpbox.westeurope.cloudapp.azure.com -Force -Verbose
-Set-Item wsman:localhost\client\trustedhosts -value * -Force
-
 $Password = $DomainAdminPassword | ConvertTo-SecureString -AsPlainText  -Force 
 $cred = New-Object System.Management.Automation.PSCredential $Username, $Password
 
@@ -34,8 +30,7 @@ Write-Verbose "Running Pester"
 Invoke-Command -Session $session -ScriptBlock{Invoke-Pester C:\Windows\Temp\ -OutputFile C:\Windows\Temp\PesterTestResults.xml -OutputFormat NUnitXml} *>&1
 Copy-Item -FromSession $session C:\windows\Temp\PesterTestResults.xml -Destination $ENV:SYSTEM_DEFAULTWORKINGDIRECTORY
 
-$session = New-PSSession -ComputerName beardjumpbox.westeurope.cloudapp.azure.com -Credential $cred -UseSSL -SessionOption $so -Authentication Credssp
 
 Write-Verbose "Running SQL Install Script"
-$ICOuput = Invoke-Command -Session $session -ScriptBlock{C:\Windows\Temp\SQLInstall.ps1} *>&1
+$ICOuput = Invoke-Command -Session $session -ScriptBlock{C:\Windows\Temp\SQLInstall.ps1 -DomainAdminPassword $Using:DomainAdminPassword } *>&1
 Write-Verbose "SQL Install Output is -$ICOutput"
